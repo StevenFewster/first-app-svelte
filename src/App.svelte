3319ec6d27e2c6de9fe738ede11fa00feff8b880
steven.j.fewster <steven.j.fewster@uk.ey.com>
2022-10-07 15:15:14 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let name;
+	export let age;
+
+	$: uppercaseName = name.toUpperCase();
+
+	function incrementAge() {
+		age += 1;
+	}
+
+	function changeName() {
+		name = 'monkey';
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Change Age</button>
+<button on:click="{changeName}">Change Name</button>
